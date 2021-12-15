b5a9c35d6334b56e9296e11ad6683a433521822d
Your Name <mohamedabotyear@gmail.com>
2021-12-16 01:00:34 +0200

    chapter 22 string

---
+++ Strings and Formatting/Strings and Formatting/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Strings_and_Formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] data = { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd' };
+            String converter = new String('5', 5);//repeat char n times
+            Console.WriteLine(converter);
+            String converter1 = new String(data, 0, 5);
+            Console.WriteLine(converter1);
+            string name = "ali";
+            string id = "1&1";
+            Console.WriteLine(name == id);
+            Console.WriteLine(name.CompareTo(id));
+            string a = "a", b = "A";
+            Console.WriteLine(String.Compare(a, b, true));
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(String.Equals(a, b, StringComparison.OrdinalIgnoreCase));
+            int result = String.Compare(a, b, StringComparison.CurrentCulture);
+            results(result);
+            result = String.Compare(a, b, StringComparison.Ordinal);
+            results(result);
+            #region split and join
+            string str = "One if by land, two if by sea.";
+            char[] seps = { ' ', '.', ',' };
+            // Split the string into parts.
+            string[] parts = str.Split(seps);
+            Console.WriteLine("Pieces from split: ");
+            for (int i = 0; i < parts.Length; i++)
+                Console.WriteLine(parts[i]);
+            // Now, join the parts.
+            string whole = String.Join(" | ", parts);
+            Console.WriteLine("Result of join: ");
+            Console.WriteLine(whole);
+            parts = str.Split(seps, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Pieces from split: ");
+            for (int i = 0; i < parts.Length; i++)
+                Console.WriteLine(parts[i]);
+            #endregion
+            Console.WriteLine(a.PadLeft(5));// add  5 spaces from left 
+            Console.WriteLine(a.PadLeft(5, '/'));
+        }
+
+
+        public static void results(int result)
+        {
+            if (result < 0)
+                Console.WriteLine("A greater than a");
+            else
+                Console.WriteLine("a greater than A");
+        }
+    }
+
+}
---
+++ exploringSystemNamespace/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+ #region  math
+Console.WriteLine(Math.BigMul(10,20));//avoid overflow
+Console.WriteLine(Math.Ceiling(12.2));
+Console.WriteLine(Math.Floor(12.2));
+Console.WriteLine(Math.DivRem(10,3));//calculate divid result and reminder
+Console.WriteLine(Math.IEEERemainder(10,3));
+Console.WriteLine(Math.Exp(3));// e ^ n
+Console.WriteLine(Math.Round(12.6));
+Console.WriteLine(Math.Round(12.6886,3));
+Console.WriteLine(Math.Round(11.5,MidpointRounding.ToEven));//will return 12 approximate to nearest event number 
+Console.WriteLine(Math.Round(11.5,MidpointRounding.AwayFromZero));
+Console.WriteLine(Math.Round(11.5,MidpointRounding.ToZero));
+Console.WriteLine(Math.Sign(0));
+
+#endregion
+#region structure
+Single f;
+f = new float();
+f = 23.6f;
+Console.WriteLine(f);
+Console.WriteLine(float.Epsilon);
+decimal val = new decimal(115,2,2,false,0);
+Console.WriteLine(val);
+char ca = 'a';
+Console.WriteLine(char.GetNumericValue('5'));
+Console.WriteLine(char.IsLetter(ca));
+int[]data = {1,15,2,4,10,22};
+int[] data1 = (int[])data.Clone();
+data1[0] = 4;
+data[0]=0;
+//decimal[]dedata = Array.ConvertAll<int,decimal>(data, dedata);
+Console.WriteLine(data[0]+"  "+data1[0]);
+
+#endregion
