4da740cd73443ee0d8eae5f0b4f1d1852b21c031
mohamedabotir <mohamedabotyear@gmail.com>
2023-06-09 16:22:12 +0300

    Asynchronouns with thread

--- Topics/ASyncPatterns/ASyncPatterns/Program.cs
+++ Topics/ASyncPatterns/ASyncPatterns/Program.cs
@@ -6,27 +6,53 @@
         {
             #region Synchronous version
 
-            Console.WriteLine("Synchronous  Version  ");
-            Method1();
-            Method2();
-            Console.WriteLine("Main has Finished  ");
+            //Console.WriteLine("Synchronous  Version  ");
+            //Method1();
+            //Method2();
+            //Console.WriteLine("Main has Finished  ");
+            //  static void Method2()
+            //{
+            //    Console.WriteLine("Method 1 has Started");
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Method 1 has Finished");
+            //}
 
+            //  static void Method1()
+            //{
+            //    Console.WriteLine("Method 1 has Started");
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Method 1 has Finished");
+
+            //}
+            #endregion 
+            #region ASynchronous version
+
+            Console.WriteLine("ASynchronous  Version  ");
+             
+            var thread1 = new Thread(e =>
+            {
+                Console.WriteLine("Method 1 has Started");
+                Thread.Sleep(1000);
+                Console.WriteLine("Method 1 has Finished");
+            });
+            thread1.Start();    
+
+            Thread.Sleep(10);
+            Method2();
+            thread1.Join();
+            Console.WriteLine("End Main().");
+
+
+           
             #endregion
         }
-
-        private static void Method2()
+        static void Method2()
         {
-            Console.WriteLine("Method 1 has Started");
-            Thread.Sleep(1000);
-            Console.WriteLine("Method 1 has Finished");
+            Console.WriteLine("Method 2 has Started");
+            Thread.Sleep(10);
+            Console.WriteLine("Method 2 has Finished");
         }
 
-        private static void Method1()
-        {
-            Console.WriteLine("Method 1 has Started");
-            Thread.Sleep(1000);
-            Console.WriteLine("Method 1 has Finished");
 
-        }
     }
 }
