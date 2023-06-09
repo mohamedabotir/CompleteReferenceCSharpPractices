04ddd247c6d9f16383e0d00101743c29bb17d113
mohamedabotir <mohamedabotyear@gmail.com>
2023-06-09 15:54:36 +0300

    AsyncPatterns - start with Synchronous

---
+++ Topics/ASyncPatterns/ASyncPatterns/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ASyncPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Synchronous version
+
+            Console.WriteLine("Synchronous  Version  ");
+            Method1();
+            Method2();
+            Console.WriteLine("Main has Finished  ");
+
+            #endregion
+        }
+
+        private static void Method2()
+        {
+            Console.WriteLine("Method 1 has Started");
+            Thread.Sleep(1000);
+            Console.WriteLine("Method 1 has Finished");
+        }
+
+        private static void Method1()
+        {
+            Console.WriteLine("Method 1 has Started");
+            Thread.Sleep(1000);
+            Console.WriteLine("Method 1 has Finished");
+
+        }
+    }
+}
