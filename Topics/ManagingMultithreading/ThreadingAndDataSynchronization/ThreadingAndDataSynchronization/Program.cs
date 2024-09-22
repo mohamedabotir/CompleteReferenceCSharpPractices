3d34f3f43ddbc4f1060ca33787452ca342945e4b
mohamedabotir <mohamedabotyear@gmail.com>
2024-09-22 17:22:11 +0300

    add comments for diff between Task.Run and Task.Factory.StartNew

---
+++ Topics/ManagingMultithreading/ThreadingAndDataSynchronization/ThreadingAndDataSynchronization/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// auto reset
+
+using ThreadingAndDataSynchronization;
+
+//new AutoReset();// execution sync
+
+new JoinSync(); // join thread to main to acheive execution sync
+
+//Data Sync do with lock for critical section
+
+   
+/*
+ *
+  Task.Run vs Task.Factory.startnew 
+  Task.Factory.StartNew(A, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);   = Task.Run(A)
+  the main different is Task.Run always run task as DenychildAttach
+ * 
+ */
