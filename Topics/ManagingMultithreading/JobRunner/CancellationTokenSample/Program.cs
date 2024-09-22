1e99df2a1bac735c9c608a33e077cd620e129c60
mohamedabotir <mohamedabotyear@gmail.com>
2024-09-22 16:24:18 +0300

    add first sample to managing multithreading , cancellationToken

---
+++ Topics/ManagingMultithreading/JobRunner/CancellationTokenSample/Program.cs
@@ -0,0 +1,44 @@
+﻿  var LongLifeRunningInMin = TimeSpan.FromMinutes(60);
+  var ShortLifeRunningInMin=TimeSpan.FromMinutes(1);
+  var cancellationTokenSource = new CancellationTokenSource();
+  ISync booking = new BookingBusiness();
+  var bookingJob =  new Task(()=>SyncJobs(booking.Sync,ShortLifeRunningInMin,cancellationTokenSource.Token),TaskCreationOptions.LongRunning);
+   Console.WriteLine("Press any key to stop...");
+   bookingJob.Start();
+   Console.ReadKey();
+  cancellationTokenSource.Cancel();
+  bookingJob.Wait();
+  
+  
+ void SyncJobs(Action doJob,TimeSpan sleepInMin,CancellationToken cancellationToken)
+ {
+     while (!cancellationToken.IsCancellationRequested)
+     {
+         try
+         {
+             doJob();
+             Task.Delay(sleepInMin, cancellationToken);
+         }
+         catch (TaskCanceledException)
+         {
+             Console.WriteLine("Job cancelled!");
+             break;
+         }
+         catch (Exception e)
+         {
+             Console.WriteLine(e);
+         }
+     }
+ }
+
+ interface ISync
+ {
+    void Sync();
+ }
+ public class BookingBusiness : ISync
+ {
+     public void Sync()
+     {
+         Console.WriteLine("Sync:{0} on:{1}","Booking",DateTime.Today); 
+     }
+ }
