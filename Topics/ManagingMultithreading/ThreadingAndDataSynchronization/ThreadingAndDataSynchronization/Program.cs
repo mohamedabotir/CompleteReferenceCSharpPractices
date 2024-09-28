3070e9285d8c6ea941b80fa98444a7e379d3a8de
mohamedabotir <mohamedabotyear@gmail.com>
2024-09-29 01:56:41 +0300

    continuationWith

--- Topics/ManagingMultithreading/ThreadingAndDataSynchronization/ThreadingAndDataSynchronization/Program.cs
+++ Topics/ManagingMultithreading/ThreadingAndDataSynchronization/ThreadingAndDataSynchronization/Program.cs
@@ -5,7 +5,7 @@ using ThreadingAndDataSynchronization;
 
 //new AutoReset();// execution sync
 
-new JoinSync(); // join thread to main to acheive execution sync
+//new JoinSync(); // join thread to main to acheive execution sync
 
 //Data Sync do with lock for critical section
 
@@ -16,14 +16,14 @@ new JoinSync(); // join thread to main to acheive execution sync
   Task.Factory.StartNew(A, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);   = Task.Run(A)
   the main different is Task.Run always run task as DenychildAttach
  * 
- */
  Parallel.For(0,500, e =>
  {
   Console.WriteLine("Value :{0} Parallel Thread :{1}",e,Thread.CurrentThread.ManagedThreadId);
   Thread.Sleep(1000);
  });
+ */
  
- Console.WriteLine("Main Thread Finished:{0}",Thread.CurrentThread.ManagedThreadId);
+ //Console.WriteLine("Main Thread Finished:{0}",Thread.CurrentThread.ManagedThreadId);
  
  // delay make thread busy for idle status , but sleep is free thread temporary to be reuse , after period finished will continue executing again 
  
@@ -50,3 +50,15 @@ new JoinSync(); // join thread to main to acheive execution sync
     |----Task2----|                       |----Task3----|
 
   */
+  
+  
+  // continution will runs on background else if you need sync with main thread use wait();
+  Task t1 = new Task(e=>
+  {
+      Console.WriteLine("Task 1 :{0}", Thread.CurrentThread.ManagedThreadId);
+  }, TaskCreationOptions.LongRunning);
+  
+  var continution = t1.ContinueWith(e=>Console.WriteLine("StatusOf1:{0},Task 2 :{1}",e.Status, Thread.CurrentThread.ManagedThreadId));
+  t1.Start();// this also may finished after main thread as this executed on separated one
+  //continution.Wait();
+  Console.WriteLine("Main Thread Finished:{0}",Thread.CurrentThread.ManagedThreadId);   
